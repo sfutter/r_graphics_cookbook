4e9f8131ec7e0c93b3b5f78d45b92c0348657d31
Steven Futter <futter.steven@gmail.com>
2016-06-08 20:04:37 +0100

    add existing file

--- r_graphics_cookbook_ch1.R
+++ r_graphics_cookbook_ch1.R
@@ -1,2 +1,14 @@
-dir <- '/Users/stevenfutter/dropbox/kaggle/titanic'
-data <- read.csv('train.csv')
+#most common way to read a file
+data <- read.csv('/Users/stevenfutter/dropbox/kaggle/titanic/train.csv')
+data
+str(data)
+
+#strings in data are treated as factors. Makes more sense to tream them as strings
+#or characters in R terminology. 
+data <- read.csv('/Users/stevenfutter/dropbox/kaggle/titanic/train.csv', stringsAsFactors=FALSE)
+str(data)
+
+#to convert to factor
+data$Sex <- factor(data$Sex)
+str(data)
+
