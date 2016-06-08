4f9fc5a91a96a8ff0cd463e1f13e4665ec81989c
Steven Futter <futter.steven@gmail.com>
2016-06-08 20:59:24 +0100

    chapter 2 upd

--- r_graphics_cookbook_ch2.R
+++ r_graphics_cookbook_ch2.R
@@ -1,9 +1,8 @@
-#most common way to read a file
-data <- read.csv('/Users/stevenfutter/dropbox/kaggle/titanic/train.csv')
-data
-str(data)
+#built in data sets
+str(mtcars)
+str(pressure)
 
-#create a scatterplot
+# Creating a Scatterplot
 plot(mtcars$wt, mtcars$mpg)
 
 #for more advanced graphics use the ggplot2 library:
@@ -16,4 +15,29 @@ qplot(wt, mpg, data=mtcars)
 ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point()
 
 
+# Creating a Line Graph
+plot(pressure$temperature, pressure$pressure, type='l')
+
+#add points with points
+plot(pressure$temperature, pressure$pressure, type='l')
+points(pressure$temperature, pressure$pressure)
+
+#to add additional line
+lines(pressure$temperature, pressure$pressure/2, col='red')
+points(pressure$temperature, pressure$pressure/2, col='red')
+
+#with ggplot2 you can get similar results to using qplot() with geom='line'
+
+library(ggplot2)
+qplot(pressure$temperature, pressure$pressure, geom='line')
+
+#if two vectors in same data frame use:
+qplot(temperature, pressure, data=pressure, geom='line')
+#equivalent to
+ggplot(pressure, aes(x=temperature, y=pressure)) + geom_line()
+
+#line and points together 
+qplot(temperature, pressure, data=pressure, geom=c('line', 'point'))
+#equivalent to
+ggplot(pressure, aes(x=temperature, y=pressure)) + geom_line() + geom_point()
 
